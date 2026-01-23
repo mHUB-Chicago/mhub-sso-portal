25e865dc81979d20093c81ae4b0c75db0740d8b7
Admin <zerniereyes@gmail.com>
2026-01-23 17:48:00 +0800

    updated schema

--- common/schemas/user.ts
+++ common/schemas/user.ts
@@ -5,6 +5,7 @@ export const UserSchema = z.object({
   id: z.string(),
   email: z.email(),
   name: z.string(),
+  role: z.enum(['USER', 'ADMIN']),
   emailVerified: z.boolean(),
   mustResetPassword: z.boolean(),
   createdAt: z.coerce.date(),
