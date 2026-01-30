5b6dfeda9bb28c5c7e4bea8e4e596fae4b01b883
Michael Elnajami <mike@breezydev.com>
2026-01-29 20:44:35 -0600

    disabled nightly stuff for now

--- backend/src/controllers/scheduledHandler.ts
+++ backend/src/controllers/scheduledHandler.ts
@@ -4,12 +4,12 @@ import { JobType } from "./queueConsumer";
 export default async (event: ScheduledEvent, env: AppType["Bindings"], ctx: ExecutionContext) => {
   // For now, this only runs at midnight
   console.log(`Scheduled job triggered: ${event.cron}`);
-  console.log(`Enqueuing SYNC_PEOPLEVINE_EVERYTHING job`);
-  await env.QUEUE.send({
-    jobId: `${crypto.randomUUID()}-${Date.now()}`,
-    jobType: JobType.SYNC_PEOPLEVINE_EVERYTHING,
-    payload: {},
-  });
-  console.log('Deleting old login flows/sessions');
+  // console.log(`Enqueuing SYNC_PEOPLEVINE_EVERYTHING job`);
+  // await env.QUEUE.send({
+  //   jobId: `${crypto.randomUUID()}-${Date.now()}`,
+  //   jobType: JobType.SYNC_PEOPLEVINE_EVERYTHING,
+  //   payload: {},
+  // });
+  // console.log('Deleting old login flows/sessions');
   // TODO: Implement deletion of old login flows/sessions
 };
